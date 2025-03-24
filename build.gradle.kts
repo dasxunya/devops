8982be4f54d1b387f19863a3f99b5dc97d58b1f0
dasxunya <dasha.ipad.mail@gmail.com>
2025-03-24 22:31:38 +0300

    add tests

--- build.gradle.kts
+++ build.gradle.kts
@@ -36,8 +36,9 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jpa:3.4.3")
     implementation("jakarta.validation:jakarta.validation-api:3.1.1")
     implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.8.5")
-
+    testImplementation("org.mockito:mockito-core:5.16.1")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    testImplementation("org.instancio:instancio-junit:5.4.1")
 
     compileOnly("org.projectlombok:lombok:1.18.36")
     annotationProcessor("org.projectlombok:lombok:1.18.36")
--- src/main/java/itmo/devops/DevopsApplication.java
+++ src/main/java/itmo/devops/DevopsApplication.java
@@ -2,13 +2,7 @@ package itmo.devops;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@ComponentScan({"itmo.devops"})
-@EntityScan(value = "itmo.devops.model")
-@EnableJpaRepositories("itmo.devops.repository")
 @SpringBootApplication
 public class DevopsApplication {
 
